00305fab94d2c472622d985c136f8626dd086d0c
Jason <jlavery828@gmail.com>
2020-02-08 23:12:09 -0500

    before deployment

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -4,7 +4,6 @@ set -e
 
 # TODO: Set to URL of git repo.
 PROJECT_GIT_URL='https://github.com/jlavery828/profiles-rest-api.git'
-
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
 echo "Installing dependencies..."
