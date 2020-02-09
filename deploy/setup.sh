a27e3dfcc2d109a6c2bf205472db2c074c9921e8
Jason <jlavery828@gmail.com>
2020-02-08 23:12:46 -0500

    before deployment

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -5,7 +5,6 @@ set -e
 # TODO: Set to URL of git repo.
 PROJECT_GIT_URL='https://github.com/jlavery828/profiles-rest-api.git'
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
-
 echo "Installing dependencies..."
 apt-get update
 apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
